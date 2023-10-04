cd589ef62467f2a02db30444c4b2c24ec7db4069
Hatun <hatun.fpt@gmail.com>
2023-10-04 14:41:31 +0700

    youtube

---
+++ HocCungHatun/B1/B1.cpp
@@ -0,0 +1,19 @@
+//Lam quen voi C
+//Trinh bien dich
+//Cau truc chuong trinh co ban
+//comment
+//Cau lenh nhap xuat
+
+
+//Khai bao thu vien
+#include <stdio.h>
+//main
+int main(){
+	int diem;
+	printf("Hello World");
+	scanf("%d",&diem);
+	printf("%d",diem);
+	
+	return 0;
+}
+
