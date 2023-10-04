90124e1e4e43fe2bdeeb0fb1ff234d7daea30c81
Hatun <hatun.fpt@gmail.com>
2023-10-04 15:33:51 +0700

    tuhoc

--- ASM/ASM.cpp
+++ ASM/ASM.cpp
@@ -359,6 +359,19 @@ int main()
     case 6:
     {
         printf("\nChuc mung ban da chon thanh cong chuc nang so 6: \n\tVay tra gop");
+        //khai bao
+        float RATE=5/100;
+        float loan;
+        //nhap xuat
+        printf("Moi ban nhap so tien can vay : ");
+        scanf("%d",&loan);
+        //run
+        float tienlai;
+        for(int i=1;i<=12;i++){
+        	printf("\nThang %d so tien ban can phai thanh toan la: %d");
+        	tienlai=
+		}
+        
         break;
     }
     // Chuc nang 7
---
+++ HocCungHatun/B1/mang.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+	printf("\nChuc mung ban da chon thanh cong chuc nang so 6: \n\tVay tra gop");
+        //khai bao
+        float RATE=5/100;
+        float loan;
+        float tientra;
+        //nhap xuat
+        printf("Moi ban nhap so tien can vay : ");
+        scanf("%d",&loan);
+        tientra=loan/12;
+        //run
+        float tienlai;
+        for(int i=1;i<=12;i++){
+        	printf("\nThang %d so tien ban can phai thanh toan la: %d");
+        	tienlai=tong*RATE;
+        	
+        	
+		}
+	return 0;
+}
