c9debfa81dbbe2dafbd8551bfcdad1fe3dcd913b
Hatun <hatun.fpt@gmail.com>
2023-10-06 16:59:24 +0700

    fix

--- ASM/b6asm.cpp
+++ ASM/b6asm.cpp
@@ -6,11 +6,11 @@ int main(){
     float tienvay;
     float RATE=0.05;
     float tienlai=0;
-    float tiengocphaithanhtoan=1000000;
     float tienphaithanhtoan=0;
     //nhap
     printf("Nhap so tien muon vay: ");
     scanf("%f",&tienvay);
+    float tiengocphaithanhtoan=tienvay/12;
     //in 
     for(int i=1;i<=12;i++){
         printf("\n\tThang thu %d:",i);
