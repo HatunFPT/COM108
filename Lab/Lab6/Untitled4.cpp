3b9e7facbceb2859f35d9d1f4a6ee38a02c6f1fc
Hatun <hatun.fpt@gmail.com>
2023-10-09 16:11:38 +0700

    lab

---
+++ Lab/Lab6/Untitled4.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+	int n,m;
+	printf("Moi ban nhap so cot cua mang: ");
+	scanf("%d",&n);
+	printf("Moi ban nhap so hang cua mang: ");
+	scanf("%d",&m);
+	int mang2chieu[n][m]
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			printf("Moi nhap phan tu o vi tri cot %d, hang %d :",n,m);
+			scanf("%d",mang2chieu[i][j]);
+		}
+	}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			printf("");
+			
+		}
+	}
+	return 0;
+}
+
---
+++ TrenLop/B13/test.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+	for (int i = 1; i < 3; i++) {
+        printf("hello %d\n", i);
+    }
+	return 0;
+}
+
